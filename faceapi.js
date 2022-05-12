58a60556e065f8783ca13f363cb85ee2aba0ac58
SanyamSavla <61912569+SanyamSavla@users.noreply.github.com>
2022-05-12 17:45:03 +0530

    GBM.. final push

--- faceapi.js
+++ faceapi.js
@@ -68,7 +68,7 @@ function loadLabeledImages() {
     labels.map(async (label) => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`);//fetching image for recognition
+        const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`); //fetching image for recognition
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
         // console.log(label + i + JSON.stringify(detections))
         descriptions.push(detections.descriptor);
